f0781912ff186d46de68592f680cf9666da65bd8
barlog1983 <osipovp@list.ru>
2018-04-26 23:12:04 +0300

    f

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -7,8 +7,6 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
-//        wd.findElement(By.name("selected{}")).click();
-//        wd.findElement(By.name("delete")).click();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
