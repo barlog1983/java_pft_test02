45cadaf39c7aafd8281a0d208a102a3396005acf
barlog1983 <osipovp@list.ru>
2018-05-01 15:34:11 +0300

    Exc_7_Run_tests_in_different_browsers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/UserHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/UserHelper.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.UserData;
 
 public class UserHelper extends HelperBase {
 
-  public UserHelper(FirefoxDriver wd) {
+  public UserHelper(WebDriver wd) {
     super(wd);
   }
 
